31288d2469614746a26c3a5c7068adda47aa1694
wenyuabby <wenyuabby@gmail.com>
2016-11-16 14:09:48 +0000

    Display of evaluation result

--- DTEvaluation.m
+++ DTEvaluation.m
@@ -10,5 +10,6 @@ cf_matrix = confusionMatrixForDT(x,y);
 
 output = recallPrecision(cf_matrix,y);
 
-disp('      Ave       E1         E2         E3          E4         E5        E6');
+disp('      Ave       E1        E2        E3        E4         E5        E6');
 disp(output);
+
