40297e6bd75b10559a86346c88f38c9a19e2d4fc
Ivy Lin <littlebugivy@gmail.com>
2016-11-16 11:49:46 +0000

    input change

--- drawAllEmotionTrees.m
+++ drawAllEmotionTrees.m
@@ -1,11 +1,10 @@
-function drawAllEmotionTrees()
+function drawAllEmotionTrees(raw_data,target_labels,emo_label)
 % This function is used to draw decision trees for
 % all the emotions with whole data set
 load('emotions_data_66.mat');
 
-for i = 1:6
-    ft_lb = datatrans(x,y,i);
-    tree = createDtree(ft_lb,i,0);
-    DrawDecisionTree(tree,'Emotion'+string(i));
-end
+ft_lb = datatrans(raw_data,target_labels,emo_label);
+tree = createDtree(ft_lb,emo_label,0);
+DrawDecisionTree(tree,'Emotion'+string(emo_label));
+
 
